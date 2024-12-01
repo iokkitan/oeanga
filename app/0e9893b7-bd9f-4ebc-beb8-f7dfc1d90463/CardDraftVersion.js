3630a148e4ac0171610e93dea9230dbd4b04fed3
Tân Io̍k-kî <khuihoat@iokkitan.com>
2024-12-01 14:37:09 +0800

    Kái jī tōa-sè pān, khò jī sò͘-liōng

--- app/0e9893b7-bd9f-4ebc-beb8-f7dfc1d90463/CardDraftVersion.js
+++ app/0e9893b7-bd9f-4ebc-beb8-f7dfc1d90463/CardDraftVersion.js
@@ -1,5 +1,17 @@
 import * as React from "react";
 
+function getTextSize(text) {
+  return [
+    { size: "9xl", maxL: 7 },
+    { size: "8xl", maxL: 11 },
+    { size: "7xl", maxL: 13 },
+    { size: "6xl", maxL: 17 },
+    { size: "5xl", maxL: 23 },
+    { size: "4xl", maxL: 29 },
+    { size: "3xl", maxL: Number.MAX_SAFE_INTEGER },
+  ].find(({ maxL }) => text.length < maxL).size;
+}
+
 export default function CardDraftVersion({
   fontFamily,
   fontVariant,
@@ -13,21 +25,23 @@ export default function CardDraftVersion({
   cardBodyTw,
   imageTw,
   stackTw,
-  headingTw,
+  // headingTw,
   textTw,
   cardFooterTw,
   footerTextTw,
 }) {
+  const headingTw = `w-5/12 border-r-2 border-gray-200 pr-6 font-bold text-gray-900 text-center text-${getTextSize(
+    heading
+  )}`;
+
   return (
     <div className="w-full h-full flex flex-col p-8 bg-white">
       <div className="relative flex">
-        <span className="w-3/12 border-r-2 border-gray-200 pr-6 text-9xl font-bold text-gray-900 text-center">
-          {heading}
-        </span>
-        <div className="w-9/12 flex flex-col pl-6 items-stretch">
+        <span className={headingTw}>{heading}</span>
+        <div className="w-7/12 flex flex-col pl-6 items-stretch">
           {text.split("\n").map((line, index) => (
             <React.Fragment key={index}>
-              <span className="py-2 text-3xl text-gray-600">{line}</span>
+              <span className={`py-2 text-gray-600 text-${getTextSize(line)}`}>{line}</span>
             </React.Fragment>
           ))}
         </div>
